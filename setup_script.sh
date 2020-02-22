105f942b40129a1e98bb186b7e19febc69770fdb
Ian Shaw <ianedwardshaw@gmail.com>
2020-02-22 14:18:35 -0500

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+cp my_vimrc.txt ~/.vimrc
 sudo apt-get install python35 tmux python35-devel gcc gdb -y
 sudo pip3 install --upgrade pip
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
 ./setup.sh
-cp my_vimrc.txt ~/.vimrc
